572224256188a48c911ca9444d37cd2822c479c8
unknown <amohamed@MAWARDYWSDE01.Elmawardy.local>
2020-05-05 14:17:18 +0200

    project,models

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+# docker-compose run app sh -c "python manage.py test"
+# docker-compose run app sh -c "python manage.py test && flake8"
+# docker-compose run app sh -c "python manage.py startapp core"
+# docker-compose run app sh -c "python manage.py makemigrations core "
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL ='core.User'
