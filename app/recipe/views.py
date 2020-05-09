b4fb358aa95b064f5740ec26079a33a2577251e8
unknown <amohamed@MAWARDYWSDE01.Elmawardy.local>
2020-05-09 12:47:45 +0200

    recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,13 +4,15 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 # Create your views here.
 
 
-class TagViewSet(viewsets.GenericViewSet,mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """docstring for TagViewSet.manage tags in db"""
     authentication_classes =(TokenAuthentication,)
     permission_classes= (IsAuthenticated,)
@@ -20,3 +22,25 @@ class TagViewSet(viewsets.GenericViewSet,mixins.ListModelMixin):
     def get_queryset(self):
         """return objectsforcurrent user authentcated only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self,serializer):
+        """create new inedient to prevent AssertionError: 405 != 400"""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self,serializer):
+        """create new  ingredfient"""
+        serializer.save(user=self.request.user)
