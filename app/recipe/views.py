30b83e9682113c2a851d732c17b6755b3bc693f9
unknown <amohamed@MAWARDYWSDE01.Elmawardy.local>
2020-05-09 13:22:51 +0200

    refactor tags and ingredient

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,15 +9,12 @@ from core.models import Tag, Ingredient
 from recipe import serializers
 # Create your views here.
 
-
-class TagViewSet(viewsets.GenericViewSet,
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
                  mixins.ListModelMixin,
                  mixins.CreateModelMixin):
-    """docstring for TagViewSet.manage tags in db"""
+    "base viewset for user owned recipe attributes"
     authentication_classes =(TokenAuthentication,)
     permission_classes= (IsAuthenticated,)
-    queryset =Tag.objects.all()
-    serializer_class =serializers.TagSerializer
 
     def get_queryset(self):
         """return objectsforcurrent user authentcated only"""
@@ -28,19 +25,47 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
+class TagViewSet(BaseRecipeAttrViewSet):
+    """docstring for TagViewSet.manage tags in db"""
+    queryset =Tag.objects.all()
+    serializer_class =serializers.TagSerializer
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """Manage ingredients in the database"""
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
 
-    def get_queryset(self):
-        """Return objects for the current authenticated user only"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self,serializer):
-        """create new  ingredfient"""
-        serializer.save(user=self.request.user)
+# class TagViewSet(viewsets.GenericViewSet,
+#                  mixins.ListModelMixin,
+#                  mixins.CreateModelMixin):
+#     """docstring for TagViewSet.manage tags in db"""
+#     authentication_classes =(TokenAuthentication,)
+#     permission_classes= (IsAuthenticated,)
+#     queryset =Tag.objects.all()
+#     serializer_class =serializers.TagSerializer
+#
+#     def get_queryset(self):
+#         """return objectsforcurrent user authentcated only"""
+#         return self.queryset.filter(user=self.request.user).order_by('-name')
+#
+#     def perform_create(self,serializer):
+#         """create new inedient to prevent AssertionError: 405 != 400"""
+#         serializer.save(user=self.request.user)
+#
+#
+# class IngredientViewSet(viewsets.GenericViewSet,
+#                         mixins.ListModelMixin,
+#                         mixins.CreateModelMixin):
+#     """Manage ingredients in the database"""
+#     authentication_classes = (TokenAuthentication,)
+#     permission_classes = (IsAuthenticated,)
+#     queryset = Ingredient.objects.all()
+#     serializer_class = serializers.IngredientSerializer
+#
+#     def get_queryset(self):
+#         """Return objects for the current authenticated user only"""
+#         return self.queryset.filter(user=self.request.user).order_by('-name')
+#
+#     def perform_create(self,serializer):
+#         """create new  ingredfient"""
+#         serializer.save(user=self.request.user)
